2aaf42a0d0cb780cbadea61032043ab9a378a4a0
Alberto Gómez <albertogomrod5@gmail.com>
2023-03-31 13:58:10 +0200

    Img responsive añadida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import sentadillabyn from "../assets/sentadillabyn.jpg";
 
 function Home() {
   return (
     <div>
       <section id="seccion1-home">
         <div>
-          <h4>
-            ¿Quieres formar parte de e-Train?
-          </h4>
-          <button className= "button-s1" >Conocer más</button>
-          <button className= "button-s1">¡Apúntate!</button>
+          <h4>¿Quieres formar parte de e-Train?</h4>
+          <button className="button-s1">Conocer más</button>
+          <button className="button-s1">¡Apúntate!</button>
         </div>
       </section>
       <section>
-      <div>
-        <h4 id="num-etrains">51</h4>
-        <p>e-Trainers</p>
-      </div>
+        <div>
+          <h4 id="num-etrains">51</h4>
+          <p>e-Trainers</p>
+        </div>
         <div>
           <h4>El bueno de Antoñito haciendo cosas buenas para la gente </h4>
           <p>
@@ -28,21 +27,25 @@ function Home() {
         </div>
       </section>
       <section id="seccion3-home">
-        <div>
-          <h4>¡Hazlo! </h4>
-          <p>Haz un trabajo por el que sientas orgullo. Da lo mejor
-          de ti. Diviértete haciéndolo. Sé valiente y empezarás a notar los resultados. Ama lo que haces.</p> 
-        </div>
-        <div>
-            <img src="" alt="img-sentadilla" />
-        </div>
+        <h4>¡Hazlo!</h4>
+        <p>
+          Haz un trabajo por el que sientas orgullo. Da lo mejor de ti.
+          Diviértete haciéndolo. Sé valiente y empezarás a notar los resultados.
+          Ama lo que haces.
+        </p>
       </section>
-      <section>
-        <div>
-            <h4>Acerca de e-Train</h4>
-            <p>El puto entrenamiento personal de Antoñito y ahora un lorem ipsum que te vas a cagar.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, eligendi a? At recusandae magnam repudiandae iure tempora ab quasi id dolore. Neque enim animi quam cum sit facere laborum dolor!</p>
-        </div>
+      <div>
+        <img  className= "img-centro-vertical" src={sentadillabyn} alt="sentadilla-byn" />
+      </div>
+      <section id="seccion4-home">
+        <h4>Acerca de e-Train</h4>
+        <p>
+          El puto entrenamiento personal de Antoñito y ahora un lorem ipsum que
+          te vas a cagar. Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Nesciunt, eligendi a? At recusandae magnam repudiandae iure
+          tempora ab quasi id dolore. Neque enim animi quam cum sit facere
+          laborum dolor!
+        </p>
       </section>
     </div>
   );
