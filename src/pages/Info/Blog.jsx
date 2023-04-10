7eac7c9e0fbcc6e1847a420ed81fe51b7a288a17
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-10 16:25:13 +0200

    Página blog

--- src/pages/Info/Blog.jsx
+++ src/pages/Info/Blog.jsx
@@ -1,8 +1,41 @@
+import { Link } from "react-router-dom";
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div>
+      <div id="seccion-blog">
+        <h2>eTrain Blog</h2>
+        <h5>
+          Aquí te cuento todo lo que sé. Podrás encontrar la solución a tus
+          preguntas y dudas más comunes.
+        </h5>
+      </div>
+
+      <div>
+        <h4>Titulo ejemplo</h4>
+        <h6>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+          iusto neque voluptatem eius deserunt doloribus.
+        </h6>
+        <Link to= "">Leer más</Link>
+         <hr />
+        <h4>Titulo ejemplo</h4>
+        <h6>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+          iusto neque voluptatem eius deserunt doloribus.
+        </h6>
+        <Link to= "">Leer más</Link>
+        <hr />
+        <h4>Titulo ejemplo</h4>
+        <h6>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
+          iusto neque voluptatem eius deserunt doloribus.
+        </h6>
+        <Link to= "">Leer más</Link>
+      </div>
+      <br />
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
