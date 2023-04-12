524bd4451f86f20f2dc22a712f149a25bf7ab169
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-12 15:36:05 +0200

    Navbar responsive terminado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import {NavLink} from "react-router-dom"
+import { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import navLogo from "../assets/logoipsum-218.svg"
+import navBurger from "../assets/burger-bar.png"
 
-function Navbar() {
+const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
-    <div>
-    <img src="" alt="logo-etrain" />
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/que-es-etrain">¿Qué es eTrain?</NavLink>
-    <NavLink to="/servicios">Servicios</NavLink>
-    <NavLink to="/blog">Blog</NavLink>
-    <NavLink to="/contacto">Contacto</NavLink>
-
-    </div>
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">
+        <img
+          src={navLogo}
+          alt="navLogo"
+        />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+        <img id="hamburguer"
+          src={navBurger}
+          alt="navBurger"
+        />
+        </div>
+        <div className={`nav-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/blog">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
