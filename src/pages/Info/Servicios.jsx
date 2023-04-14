3fb5638213ab20ba199b93144a0087cfb87dd017
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-14 13:51:50 +0200

    Página servicios empezada

--- src/pages/Info/Servicios.jsx
+++ src/pages/Info/Servicios.jsx
@@ -1,8 +1,61 @@
-
 function Servicios() {
   return (
-    <div>Servicios</div>
-  )
+    <div id="servicios">
+      <div>
+      <h6>Mis servicios</h6>
+        <h2>Rendimiento, Readaptación, Preparación y Salud</h2>
+      </div>
+      <div id= "pilares-etrain">
+        <div >
+          <h3 className="numero-pilar">01</h3>
+          <hr />
+          <h4 className="h4-pilares">Rendimiento</h4>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo a
+            debitis et.
+          </p>
+        </div>
+        <div>
+          <h3 className="numero-pilar">02</h3>
+          <hr />
+          <h4 className="h4-pilares">Readaptación</h4>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo a
+            debitis et.
+          </p>
+        </div>
+        <div>
+          <h3 className="numero-pilar">03</h3>
+          <hr />
+          <h4 className="h4-pilares">Preparación</h4>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo a
+            debitis et.
+          </p>
+        </div>
+        <div>
+          <h3 className="numero-pilar">04</h3>
+          <hr />
+          <h4 className="h4-pilares">Salud</h4>
+          <hr />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo a
+            debitis et.
+          </p>
+        </div>
+      </div>
+      <br />
+      <div>
+        <h2>¿Te gustaría comenzar ahora con el cambio?</h2>
+        <h4>¡Hablemos!</h4>
+        <p>holaantoñito@etrain.com</p>
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
-export default Servicios
+export default Servicios;
