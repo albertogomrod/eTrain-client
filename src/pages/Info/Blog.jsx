6fead15cc39649bf768613e26a0fde8e2d185de1
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-20 15:44:01 +0200

    Vista de blogs terminadas

--- src/pages/Info/Blog.jsx
+++ src/pages/Info/Blog.jsx
@@ -1,39 +1,52 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { listaEntradasBlogService } from "../../services/blog.services";
 
 function Blog() {
+  const navigate = useNavigate();
+
+  const [allBlogs, setAllBlogs] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setIsFetching(true);
+    try {
+      const response = await listaEntradasBlogService();
+      setIsFetching(false);
+      setAllBlogs(response.data);
+      console.log(response.data)
+    } catch (error) {
+      navigate("/error");
+    }
+  };
+
+  if (isFetching === true) {
+    return (
+      <p>Cargando...</p>
+    );
+  }
+
   return (
     <div>
-      <div id="seccion-blog">
-        <h2>eTrain Blog</h2>
-        <h5>
-          Aquí te cuento todo lo que sé. Podrás encontrar la solución a tus
-          preguntas y dudas más comunes.
-        </h5>
-      </div>
-
-      <div>
-        <h4>Titulo ejemplo</h4>
-        <h6>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-          iusto neque voluptatem eius deserunt doloribus.
-        </h6>
-        <Link to= "">Leer más</Link>
-         <hr />
-        <h4>Titulo ejemplo</h4>
-        <h6>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-          iusto neque voluptatem eius deserunt doloribus.
-        </h6>
-        <Link to= "">Leer más</Link>
-        <hr />
-        <h4>Titulo ejemplo</h4>
-        <h6>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias,
-          iusto neque voluptatem eius deserunt doloribus.
-        </h6>
-        <Link to= "">Leer más</Link>
-      </div>
-      <br />
+      <h2>eTrain Blog</h2>
+      <h3>
+        Aquí te cuento todo lo que sé. Podrás encontrar la solución a tus
+        preguntas y dudas más comunes.
+      </h3>
+      {allBlogs.length === 0 ? <h4>No hay blogs disponibles</h4> : null}
+      {allBlogs.map((cadaBlog) => {
+        return (
+          <div>
+            <h4>{cadaBlog.title}</h4>
+            <h5>{cadaBlog.subtitle}</h5>
+            <Link to={`/blog/${cadaBlog._id}`}> Leer mas</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
