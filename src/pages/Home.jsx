71de4e4e27a056382e1b74d48251f81fb948c30c
Alberto Gómez <albertogomrod5@gmail.com>
2023-04-27 10:58:35 +0200

    Lista de blogs en la home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { listaEntradasBlogService } from "../services/blog.services";
+import { Link } from "react-router-dom";
 
 import sentadillabyn from "../assets/sentadillabyn.jpg";
-import CountUp from 'react-countup';
+import CountUp from "react-countup";
 
 function Home() {
+  const navigate = useNavigate();
+
+  const [allBlogs, setAllBlogs] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    setIsFetching(true);
+    try {
+      const response = await listaEntradasBlogService();
+      setIsFetching(false);
+      setAllBlogs(response.data);
+      console.log(response.data);
+    } catch (error) {
+      navigate("/error");
+    }
+  };
+
+  if (isFetching === true) {
+    return <p>Cargando...</p>;
+  }
+
   return (
     <div>
       <section id="seccion1-home">
@@ -15,7 +45,9 @@ function Home() {
       </section>
       <section>
         <div>
-          <h4 id="num-etrains"><CountUp end={51} duration={4} /></h4>
+          <h4 id="num-etrains">
+            <CountUp end={51} duration={4} />
+          </h4>
           <p>e-Trainers</p>
         </div>
         <div>
@@ -52,6 +84,20 @@ function Home() {
           tempora ab quasi id dolore. Neque enim animi quam cum sit facere
           laborum dolor!
         </p>
+      </section>
+      <section>
+        <h4>eTrain Blog</h4>
+        {allBlogs.length === 0 ? <h4>No hay blogs disponibles</h4> : null}
+        {allBlogs.map((cadaBlog) => {
+          return (
+            <div>
+              <img src={cadaBlog.photo} alt="fotoportada-blog" width={"60%"} />{" "}
+              <Link to={`/blog/${cadaBlog._id}`}>
+                <h4>{cadaBlog.title}</h4>
+              </Link>
+            </div>
+          );
+        })}
       </section>
     </div>
   );
