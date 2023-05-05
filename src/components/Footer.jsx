58133c59494eed074e46047469236101179729af
Alberto Gómez <albertogomrod5@gmail.com>
2023-05-05 23:34:14 +0200

    Footer completado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-
+import navLogo from "../assets/logoipsum-218.svg";
 function Footer() {
   return (
     <div className="section">
       <footer className="top">
-        <img src="" alt="logo-etrain" />
+        <img src={navLogo} alt="logo-etrain" />
         <div className="links">
           <div>
-            <h2>Plataform</h2>
-            <a>Directus Core</a>
+            <h2>Contacta con eTrain</h2>
+            <a>Via email</a>
           </div>
           <div>
-            <h2>Cloud</h2>
-            <a>Directus Core</a>
+            <h2>Redes Sociales</h2>
+            <a href="https://www.instagram.com/etrain_pt/">Instagram</a>
+            <a href="https://twitter.com/home">Twitter</a>
           </div>
         </div>
       </footer>
       <footer className="bottom">
         <div className="legal">
-          <span> 2023 Aal rigths reserverd</span>
-          <a href="">License</a>
+          <span> 2023 Todos los derechos reservados</span>
+          <a href="">Licencia</a>
         </div>
         <div class="links">
           <a class="fa-brands fa-github"></a>
