5aca52853f21e8e53bdbed087ee8cd7e451ec07b
Alberto Gómez <albertogomrod5@gmail.com>
2023-06-07 12:25:27 +0200

    Cambios footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,27 +4,32 @@ function Footer() {
     <div className="section">
       <footer className="top">
         <img src={navLogo} alt="logo-etrain" />
+        <br />
         <div className="links">
           <div>
             <h2>Contacta con eTrain</h2>
             <a>Via email</a>
           </div>
+          <br />
           <div>
-            <h2>Redes Sociales</h2>
-            <a href="https://www.instagram.com/etrain_pt/">Instagram</a>
-            <a href="https://twitter.com/home">Twitter</a>
+            <div>
+              <h2>Redes Sociales</h2>
+              <a href="https://www.instagram.com/etrain_pt/">Instagram</a>
+              <br />
+              <a href="https://twitter.com/home">Twitter</a>
+              <br />
+              
+            </div>
           </div>
         </div>
       </footer>
       <footer className="bottom">
         <div className="legal">
-          <span> 2023 Todos los derechos reservados</span>
-          <a href="">Licencia</a>
-        </div>
-        <div class="links">
-          <a class="fa-brands fa-github"></a>
-          <a class="fa-brands fa-linkedin"></a>
-          <a class="fa-brands fa-docker"></a>
+          <a href="">Legal Notice</a>
+
+          <a href="">Privacy Police</a>
+
+          <a href="">Cookies</a>
         </div>
       </footer>
     </div>
