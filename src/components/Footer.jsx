eb7ecb2f6fb3300f18d5a7dad8665c9bbb0bfdb6
Alberto Gómez <albertogomrod5@gmail.com>
2023-06-07 13:01:05 +0200

    Contenido footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,22 +3,29 @@ function Footer() {
   return (
     <div className="section">
       <footer className="top">
-        <img src={navLogo} alt="logo-etrain" />
+        <img id="logoFooter" src={navLogo} alt="logo-etrain" />
         <br />
         <div className="links">
           <div>
-            <h2>Contacta con eTrain</h2>
+            <h2 className="titles">Madrid</h2>
+            <p>40.4356° Lat, -3.7057° Long</p>
+            <p>etrain.com</p>
+            <p>+34 688 48 83 21</p>
             <a>Via email</a>
+          </div>
+          <div>
+            <h2 className="titles">Remoto</h2>
+            <p>+34 688 48 83 21</p>
+            <a href="">etrain@gmail.com</a>
           </div>
           <br />
           <div>
             <div>
-              <h2>Redes Sociales</h2>
+              <h2 className="titles">Redes Sociales</h2>
               <a href="https://www.instagram.com/etrain_pt/">Instagram</a>
               <br />
               <a href="https://twitter.com/home">Twitter</a>
               <br />
-              
             </div>
           </div>
         </div>
