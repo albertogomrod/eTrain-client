9ea58abb5726277d619b36ba7153dee557c751cc
Alberto Gómez <albertogomrod5@gmail.com>
2023-06-15 17:33:23 +0200

    Estilo seccion 4, mejorado estilo seccion 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,47 +73,52 @@ function Home() {
         </div>
       </section>
       <section>
+        <div id="tituloSecc3">
+          <p> ETRAIN</p>
+          <hr />
+        </div>
+
         <div id="seccion3-home">
-          <div className="serviciosSecc3">
-            <p> ETRAIN</p>
-            <hr />
-          </div>
-          <h4 id="h4Secc3">¡Hazlo!</h4>
-          <p className="serviciosSecc3">
-            Haz un trabajo por el que sientas orgullo. Da lo mejor de ti.
-            Diviértete haciéndolo. Sé valiente y empezarás a notar los
-            resultados. Ama lo que haces.
-            <br />
-            <br />
-            Antoñito te hace un Lorem ipsum dolor sit amet consectetur,
-            adipisicing elit. ¡Y ni te enteras! Increible.
-          </p>
-          <div className="serviciosSecc3">
+          <div>
+            <h4>¡Hazlo!</h4>
+            <p>
+              Haz un trabajo por el que sientas orgullo. Da lo mejor de ti.
+              Diviértete haciéndolo. Sé valiente y empezarás a notar los
+              resultados. Ama lo que haces.
+              <br />
+              <br />
+              Antoñito te hace un Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. ¡Y ni te enteras! Increible.
+            </p>
             <hr />
             <p> ➯ Servicios</p>
             <hr />
           </div>
-
-          <p id="pSecc3">
-            Y en esta parte
-            <br />
-            Llega la Elena
-            <br />
-            La de los clicks
-            <br />
-            Y te mete un diseño to guapo
-            <br />Y me arregla toda la web del Antoñio
-          </p>
+          <div>
+            <p id="elenaSecc3">
+              Y en esta parte
+              <br />
+              Llega la Elena
+              <br />
+              La de los clicks
+              <br />
+              Y te mete un diseño to guapo
+              <br />Y me arregla toda la web del Antoñio
+            </p>
+          </div>
         </div>
       </section>
       <marquee width="100%" scrollamount="15">
         <h2>El bueno de Antoñito haciendo cosas buenas para la gente </h2>
       </marquee>
       <section id="seccion4-home">
-        <h4>Acerca de e-Train</h4>
+        <p id="acercaDe">Acerca de e-Train</p>
         <div id="seccion4-beneficios">
           <div>
             <p className="numeroSecc4">01.</p>
+            <hr />
+            <p className="tituloSecc4">Especialización</p>
+            <hr />
             <p>
               El puto entrenamiento personal de Antoñito y ahora un lorem ipsum
               que te vas a cagar. Lorem ipsum dolor sit amet consectetur
@@ -129,6 +134,51 @@ function Home() {
               praesentium magni veniam suscipit?
             </p>
           </div>
+          <div>
+            <p className="numeroSecc4">02.</p>
+            <hr />
+            <p className="tituloSecc4">Readaptación</p>
+            <hr />
+            <p>
+              El puto entrenamiento personal de Antoñito y ahora un lorem ipsum
+              que te vas a cagar. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Nesciunt, eligendi a? At recusandae magnam
+              repudiandae iure tempora ab quasi id dolore. Neque enim animi quam
+              cum sit facere laborum dolor! Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Doloremque iusto consequuntur
+              voluptas explicabo alias laudantium dolor illum, nisi, eius
+              eveniet atque similique fugit ullam. Molestiae corrupti doloribus
+              quo unde vero. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Vitae qui eveniet numquam quos totam perspiciatis nulla ad
+              nobis aliquid est, sequi maiores tempore ab facere natus
+              praesentium magni veniam suscipit?
+            </p>
+          </div>
+          <div>
+            <p className="numeroSecc4">03.</p>
+            <hr />
+            <p className="tituloSecc4">Flexibilidad</p>
+            <hr />
+            <p>
+              El puto entrenamiento personal de Antoñito y ahora un lorem ipsum
+              que te vas a cagar. Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Nesciunt, eligendi a? At recusandae magnam
+              repudiandae iure tempora ab quasi id dolore. Neque enim animi quam
+              cum sit facere laborum dolor! Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Doloremque iusto consequuntur
+              voluptas explicabo alias laudantium dolor illum, nisi, eius
+              eveniet atque similique fugit ullam. Molestiae corrupti doloribus
+              quo unde vero. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Vitae qui eveniet numquam quos totam perspiciatis nulla ad
+              nobis aliquid est, sequi maiores tempore ab facere natus
+              praesentium magni veniam suscipit?
+            </p>
+          </div>
+        </div>
+        <div id="saberSecc4">
+          <hr />
+          <Link to={`/que-es-etrain`}>➯ Saber más de eTrain</Link>
+          <hr />
         </div>
       </section>
       <section id="seccionBlog">
